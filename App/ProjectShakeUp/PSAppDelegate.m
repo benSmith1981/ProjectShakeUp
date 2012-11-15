03457110624451ce571c3265d3c45910a91362aa
Martin Lloyd <martin.lloyd@newsint.co.uk>
2012-11-15 16:22:27 +0000

    Download articles when app is launched from background

--- App/ProjectShakeUp/PSAppDelegate.m
+++ App/ProjectShakeUp/PSAppDelegate.m
@@ -48,7 +48,7 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    [self.viewController refresh];
+    [TSFeed getFeed:kSUN_FEED_SERVICE_KEY];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
