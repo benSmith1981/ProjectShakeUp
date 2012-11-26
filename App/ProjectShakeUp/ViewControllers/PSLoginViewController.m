f9d56c95e0ea9cb185fb776b23a0afc2de56929f
Martin Lloyd <martin.lloyd@newsint.co.uk>
2012-11-26 15:20:48 +0000

    Added facebook app ids

--- App/ProjectShakeUp/ViewControllers/PSLoginViewController.m
+++ App/ProjectShakeUp/ViewControllers/PSLoginViewController.m
@@ -65,6 +65,7 @@
 - (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
                             user:(id<FBGraphUser>)user
 {
+    NSLog(@"%@", [user objectForKey:@"email"]);
     NSString* labelFirstName = [NSString stringWithFormat:@"Hello %@!", user.first_name];    
                                 
     UIAlertView *message = [[UIAlertView alloc] initWithTitle:labelFirstName
@@ -75,8 +76,6 @@
     [message show];
     
     Debug(@"%@", labelFirstName);
-    
-    [self dismissModalViewControllerAnimated:YES];
 }
 
 - (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
